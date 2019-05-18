8ab3fa8cd2be4a0b3492cdf7ac8d48393f118d9f
skowi <szymon.skowronski@gmail.com>
2019-05-18 18:16:12 +0200

    Reset database command

---
+++ src/Command/ResetDatabaseCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\BeerMenu\Infrastructure\Resetter;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ResetDatabaseCommand extends Command
+{
+    protected static $defaultName = 'app:reset-db';
+    private $resetter;
+
+    public function __construct(Resetter $resetter)
+    {
+
+        parent::__construct();
+        $this->resetter = $resetter;
+    }
+
+    protected function configure()
+    {
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->resetter->reset();
+
+        $output->writeln('Database is clear now ');
+    }
+}
